89b9318f88dfc586a637606be959f22f6d223306
adi <adi@locallitix.com>
2026-04-25 20:27:44 +0700

    fix: premium video perf, Capacitor App exit/back handler, media notification plugins, WebView media autoplay

--- android/app/src/main/java/co/id/globalvillage/app/MainActivity.java
+++ android/app/src/main/java/co/id/globalvillage/app/MainActivity.java
@@ -1,18 +1,21 @@
 package co.id.globalvillage.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
-    public void onBackPressed() {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable media playback without user gesture (for background audio)
         WebView webView = getBridge().getWebView();
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            // Let the WebView JS handle it via popstate
-            webView.evaluateJavascript("history.back()", null);
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMediaPlaybackRequiresUserGesture(false);
         }
     }
 }
