a26c567421f9dde13bff8a8f4505fee5310a27ab
adi <adi@locallitix.com>
2026-04-25 19:45:41 +0700

    fix(android): override onBackPressed for WebView navigation, add media permissions for notification controls, update capacitor config

--- android/app/src/main/java/co/id/globalvillage/app/MainActivity.java
+++ android/app/src/main/java/co/id/globalvillage/app/MainActivity.java
@@ -1,5 +1,18 @@
 package co.id.globalvillage.app;
 
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = getBridge().getWebView();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the WebView JS handle it via popstate
+            webView.evaluateJavascript("history.back()", null);
+        }
+    }
+}
